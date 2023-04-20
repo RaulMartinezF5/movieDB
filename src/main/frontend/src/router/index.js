1a381bc5e202f3c725aa3b06e237dd64630064d1
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-04-20 11:20:56 +0200

    configurando login

--- src/main/frontend/src/router/index.js
+++ src/main/frontend/src/router/index.js
@@ -1,22 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/HomeView.vue'
+import Homeview from '../views/HomeView.vue'
+import loginView from '../views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: LoginView
+      name: 'login',
+      component: loginView
     },
-    // {
-    //   path: '/home',
-    //   name: 'home',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/HomeView.vue')
-    // },
+    {
+      path: '/home',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Homeview
+    },
     {
       path: '/mymovies',
       name: 'mymovies',
